37d19911f8290c959858205fd81f1293849509ad
René <reneskjodt333@gmail.com>
2025-12-01 12:53:44 +0100

    User Model

---
+++ Blazor/Models/User.cs
@@ -0,0 +1,10 @@
+class User
+{
+    int id { get; set; }
+    string username { get; set; }
+    string password { get; set; }
+    string email { get; set; }
+    string phoneNumber { get; set; }
+    string passHash { get; set; }
+    DateTime createdAt { get; set; }
+}
