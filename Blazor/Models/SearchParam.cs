c9d6efcdcf1ec349d0f9b273cea8c8f04052ecc5
Kasper Lundby <venoelundby@gmail.com>
2025-12-10 13:24:35 +0100

    style fixes

---
+++ Blazor/Models/SearchParam.cs
@@ -0,0 +1,16 @@
+﻿namespace Blazor.Models
+{
+    public enum filterRangeType { Pieces, Price}
+    public enum orderByType { Pieces, Price, CreatedAt }
+
+    public enum inOrderType { asc, desc }
+    public class SearchParam
+    {
+        public string SearchText { get; set; } = null;
+        public orderByType OrderBy = orderByType.CreatedAt;
+        public inOrderType InOrder = inOrderType.desc;
+        public filterRangeType FilterRange = filterRangeType.Pieces;
+        public int RangeMin { get; set; }
+        public int? RangeMax { get; set; }
+    }
+}
