5fde30985f00b6e748d11955f45403f6e3070640
René <reneskjodt333@gmail.com>
2025-12-10 11:11:07 +0100

    feat: enhance advert filtering with dynamic where clause and parameter handling

--- Blazor/Data/AdvertService.cs
+++ Blazor/Data/AdvertService.cs
@@ -8,6 +8,61 @@ public class AdvertService
         _connectionString = ConnectionString.DefaultConnection;
     }
 
+    private string GetWhereClause(bool? isSold = false, string? searchTerm = null, string? username = null, int? advertId = null)
+    {
+        List<string> conditions = new List<string>();
+        List<List<string>> OrConditions = new List<List<string>>();
+
+        string whereString = "";
+
+        if (isSold.HasValue)
+        {
+            conditions.Add($@"a.""IsSold"" = @isSold");
+        }
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            OrConditions.Add(new List<string>
+            {
+                $@"a.""Title"" ILIKE '%' || @searchTerm || '%'",
+                $@"a.""Description"" ILIKE '%' || @searchTerm || '%'"
+            });
+        }
+        if (!string.IsNullOrEmpty(username))
+        {
+            conditions.Add($@"u.""Username"" = @username");
+        }
+        if (advertId.HasValue)
+        {
+            conditions.Add($@"a.""AdvertId"" = @advertId");
+        }
+        
+        conditions.AddRange(OrConditions.Select(orGroup => "(" + string.Join(" OR ", orGroup) + ")"));
+        whereString = string.Join(" AND ", conditions);
+        return whereString;
+    }
+
+    private void AddWhereParameters(NpgsqlCommand cmd, bool? isSold = false, string? searchTerm = null, string? username = null, int? advertId = null)
+    {
+        if (isSold.HasValue)
+        {
+            cmd.Parameters.AddWithValue("isSold", isSold.Value);
+        }
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            cmd.Parameters.AddWithValue("searchTerm", searchTerm);
+        }
+        if (!string.IsNullOrEmpty(username))
+        {
+            cmd.Parameters.AddWithValue("username", username);
+        }
+        if (advertId.HasValue)
+        {
+            cmd.Parameters.AddWithValue("advertId", advertId.Value);
+        }
+    }
+
     public async Task AddAdvertAsync(Advert advert)
     {
         try
@@ -69,31 +124,8 @@ public class AdvertService
         }
     }
 
-    public async Task<int> GetAdvertCountAsync(bool? isSold = false, string? searchTerm = null)
+    public async Task<dynamic[]> GetAdvertLimits(bool? isSold = false, string? searchTerm = null)
     {
-
-        List<string> conditions = new List<string>();
-        List<List<string>> OrConditions = new List<List<string>>();
-
-        string whereString = "";
-
-        if (isSold.HasValue)
-        {
-            conditions.Add($@"""IsSold"" = @isSold");
-        }
-
-        if (!string.IsNullOrEmpty(searchTerm))
-        {
-            OrConditions.Add(new List<string>
-            {
-                $@"""Title"" ILIKE '%' || @searchTerm || '%'",
-                $@"""Description"" ILIKE '%' || @searchTerm || '%'"
-            });
-        }
-        
-        conditions.AddRange(OrConditions.Select(orGroup => "(" + string.Join(" OR ", orGroup) + ")"));
-        whereString = string.Join(" AND ", conditions);
-
         try
         {
             using var conn = new NpgsqlConnection(_connectionString);
@@ -101,57 +133,56 @@ public class AdvertService
 
             using var cmd = new NpgsqlCommand(
                 $@"SELECT 
-                    COUNT(*) 
+                    COUNT(*),
+                    MAX(a.""Price"") as MaxPrice,
+                    MAX(a.""PieceAmount"") as MaxPieceAmount
                 FROM 
-                    ""Advert"" 
+                    ""Advert"" a
                 WHERE 
-                    {whereString}",
+                    {GetWhereClause(isSold: isSold, searchTerm: searchTerm)}",
                 conn
             );
 
-            if (isSold.HasValue)
-            {
-                cmd.Parameters.AddWithValue("isSold", isSold.Value);
-            }
+            AddWhereParameters(cmd, isSold: isSold, searchTerm: searchTerm);
 
-            if (!string.IsNullOrEmpty(searchTerm))
-            {
-                cmd.Parameters.AddWithValue("searchTerm", searchTerm);
-            }
 
-            var count = (long)await cmd.ExecuteScalarAsync();
-            return (int)count;
+            var reader = await cmd.ExecuteReaderAsync();
+            if (await reader.ReadAsync())
+            {
+                int count = reader.GetInt32(0);
+                double maxPrice = reader.IsDBNull(1) ? 0 : reader.GetDouble(1);
+                int maxPieceAmount = reader.IsDBNull(2) ? 0 : reader.GetInt32(2);
+                return new dynamic[] { count, maxPrice, maxPieceAmount };
+            }
         } catch (Exception ex)
         {
-            Console.WriteLine($"Error retrieving advert count: {ex.Message}");
-            return 0;
+            Console.WriteLine($"Error retrieving advert count: {ex.Message} {ex.StackTrace}");
         }
+        return new dynamic[] { 0, 0, 0 };
     }
 
-    public async Task<List<Advert>> GetAllAdvertsAsync(int offset = 0, int limit = 100, bool? isSold = false, string? searchTerm = null)
+    public async Task<double> GetMaxAdvertPriceAsync(bool? isSold = false, string? searchTerm = null)
+    {
+        var limits = await GetAdvertLimits(isSold, searchTerm);
+        return limits[1];
+    }
+
+    public async Task<int> GetMaxAdvertPieceAmountAsync(bool? isSold = false, string? searchTerm = null)
+    {
+        var limits = await GetAdvertLimits(isSold, searchTerm);
+        return limits[2];
+    }
+
+    public async Task<int> GetAdvertCountAsync(bool? isSold = false, string? searchTerm = null)
+    {
+        var limits = await GetAdvertLimits(isSold, searchTerm);
+        return limits[0];
+    }
+
+    public async Task<List<Advert>> GetAllAdvertsAsync(int offset = 0, int limit = 100, bool? isSold = false, string? searchTerm = null, string? username = null, int? advertId = null)
     {
         var adverts = new List<Advert>();
-        List<string> conditions = new List<string>();
-        List<List<string>> OrConditions = new List<List<string>>();
-
-        string whereString = "";
-
-        if (isSold.HasValue)
-        {
-            conditions.Add($@"a.""IsSold"" = @isSold");
-        }
-
-        if (!string.IsNullOrEmpty(searchTerm))
-        {
-            OrConditions.Add(new List<string>
-            {
-                $@"a.""Title"" ILIKE '%' || @searchTerm || '%'",
-                $@"a.""Description"" ILIKE '%' || @searchTerm || '%'"
-            });
-        }
-        
-        conditions.AddRange(OrConditions.Select(orGroup => "(" + string.Join(" OR ", orGroup) + ")"));
-        whereString = string.Join(" AND ", conditions);
+        string whereString = GetWhereClause(isSold: isSold, searchTerm: searchTerm, username: username, advertId: advertId);
 
         try
         {
@@ -183,6 +214,7 @@ public class AdvertService
                 WHERE 
                     {whereString}
                 ORDER BY 
+                    a.""IsSold"" ASC,
                     a.""CreatedAt"" DESC
                 OFFSET 
                     @offset 
@@ -194,15 +226,7 @@ public class AdvertService
             cmd.Parameters.AddWithValue("offset", offset);
             cmd.Parameters.AddWithValue("limit", limit);
 
-            if (isSold.HasValue)
-            {
-                cmd.Parameters.AddWithValue("isSold", isSold.Value);
-            }
-
-            if (!string.IsNullOrEmpty(searchTerm))
-            {
-                cmd.Parameters.AddWithValue("searchTerm", searchTerm);
-            }
+            AddWhereParameters(cmd, isSold: isSold, searchTerm: searchTerm, username: username, advertId: advertId);
 
             using var reader = await cmd.ExecuteReaderAsync();
 
@@ -245,165 +269,13 @@ public class AdvertService
 
     public async Task<Advert> GetAdvertByIdAsync(int advertId)
     {
-        try
-        {
-            using var conn = new NpgsqlConnection(_connectionString);
-            await conn.OpenAsync();
-
-            using var cmd = new NpgsqlCommand(
-                @"SELECT 
-                    a.""AdvertId"", 
-                    a.""Title"", 
-                    a.""Description"", 
-                    a.""Price"", 
-                    a.""PieceAmount"", 
-                    a.""BoxDimHeight"", 
-                    a.""BoxDimWidth"", 
-                    a.""BoxDimDepth"", 
-                    a.""PuzzleDimHeight"", 
-                    a.""PuzzleDimWidth"", 
-                    a.""Picture"", 
-                    u.""UserId"",
-                    u.""Username"", 
-                    u.""Email"", 
-                    u.""PhoneNumber"", 
-                    a.""CreatedAt"", 
-                    a.""IsSold"" 
-                FROM 
-                    ""Advert"" a 
-                    LEFT JOIN ""UserAccounts"" u ON a.""UserId"" = u.""UserId""
-                WHERE 
-                    a.""AdvertId"" = @advertId",
-                conn
-            );
-
-            cmd.Parameters.AddWithValue("advertId", advertId);
-
-            using var reader = await cmd.ExecuteReaderAsync();
-
-            if (await reader.ReadAsync())
-            {
-                return new Advert
-                {
-                    AdvertId = reader.GetInt32(0),
-                    Title = reader.GetString(1), 
-                    Description = reader.GetString(2),
-                    Price = reader.GetDouble(3),
-                    PieceAmount = reader.GetInt32(4),
-                    BoxDimensions = new Vector3{
-                        Height = reader.GetDouble(5),
-                        Width = reader.GetDouble(6),
-                        Depth = reader.GetDouble(7)
-                    },
-                    PuzzleDimensions = new Vector2{
-                        Height = reader.GetDouble(8),
-                        Width = reader.GetDouble(9)
-                    },
-                    Picture = !reader.IsDBNull(10) ? reader["Picture"] as byte[] : null,
-                    User = new User { 
-                        UserId = reader.GetInt32(11),
-                        Username = reader.GetString(12), 
-                        Email = reader.GetString(13), 
-                        PhoneNumber = reader.GetString(14) 
-                    },
-                    CreatedAt = reader.GetDateTime(15),
-                    IsSold = reader.GetBoolean(16)
-                };
-            }
-            return null;
-        } catch (Exception ex)
-        {
-            Console.WriteLine($"Error retrieving advert by ID: {ex.Message}");
-            return null;
-        }
+        var adverts = await GetAllAdvertsAsync(advertId: advertId);
+        return adverts.FirstOrDefault();
     }
 
     public async Task<List<Advert>> GetAdvertsByUserAsync(string username, int offset = 0, int limit = 100)
     {
-        var adverts = new List<Advert>();
-        try
-        {
-            using var conn = new NpgsqlConnection(_connectionString);
-            await conn.OpenAsync();
-
-            using var cmd = new NpgsqlCommand(
-                @"SELECT 
-                    a.""AdvertId"", 
-                    a.""Title"", 
-                    a.""Description"", 
-                    a.""Price"", 
-                    a.""PieceAmount"", 
-                    a.""BoxDimHeight"", 
-                    a.""BoxDimWidth"", 
-                    a.""BoxDimDepth"", 
-                    a.""PuzzleDimHeight"", 
-                    a.""PuzzleDimWidth"", 
-                    a.""Picture"", 
-                    u.""UserId"",
-                    u.""Username"", 
-                    u.""Email"", 
-                    u.""PhoneNumber"", 
-                    a.""CreatedAt"", 
-                    a.""IsSold"" 
-                FROM 
-                    ""Advert"" a 
-                    LEFT JOIN ""UserAccounts"" u ON a.""UserId"" = u.""UserId""
-                WHERE 
-                    u.""Username"" = @username
-                ORDER BY 
-                    a.""IsSold"" ASC,
-                    a.""CreatedAt"" DESC
-                OFFSET 
-                    @offset 
-                LIMIT 
-                    @limit",
-                conn
-            );
-
-            cmd.Parameters.AddWithValue("username", username);
-            cmd.Parameters.AddWithValue("offset", offset);
-            cmd.Parameters.AddWithValue("limit", limit);
-
-            using var reader = await cmd.ExecuteReaderAsync();
-
-            while (await reader.ReadAsync())
-            {
-                adverts.Add(new Advert
-                {
-                    AdvertId = reader.GetInt32(0),
-                    Title = reader.GetString(1),
-                    Description = reader.GetString(2),
-                    Price = reader.GetDouble(3),
-                    PieceAmount = reader.GetInt32(4),
-                    BoxDimensions = new Vector3
-                    {
-                        Height = reader.GetDouble(5),
-                        Width = reader.GetDouble(6),
-                        Depth = reader.GetDouble(7)
-                    },
-                    PuzzleDimensions = new Vector2
-                    {
-                        Height = reader.GetDouble(8),
-                        Width = reader.GetDouble(9)
-                    },
-                    Picture = !reader.IsDBNull(10) ? reader["Picture"] as byte[] : null,
-                    User = new User
-                    {
-                        UserId = reader.GetInt32(11),
-                        Username = reader.GetString(12),
-                        Email = reader.GetString(13),
-                        PhoneNumber = reader.GetString(14)
-                    },
-                    CreatedAt = reader.GetDateTime(15),
-                    IsSold = reader.GetBoolean(16)
-                });
-            }
-            return adverts;
-        } catch (Exception ex)
-        {
-            Console.WriteLine($"Error retrieving advert by User ID: {ex.Message}");
-            return adverts;
-        }
+        return await GetAllAdvertsAsync(offset: offset, limit: limit, username: username, isSold: null);
     }
 
     public async Task<Advert> UpdateAdvertAsync(Advert advert)
