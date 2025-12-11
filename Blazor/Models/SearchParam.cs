059bc351d94103e77c054aef63225d2a0dd34130
Kasper Lundby <venoelundby@gmail.com>
2025-12-11 11:59:03 +0100

    user pagination

--- Blazor/Models/SearchParam.cs
+++ Blazor/Models/SearchParam.cs
@@ -1,16 +1,16 @@
 ﻿namespace Blazor.Models
 {
     public enum filterRangeType { Pieces, Price}
-    public enum orderByType { Pieces, Price, CreatedAt }
+    public enum orderByType { PieceAmount, Price, CreatedAt }
 
-    public enum inOrderType { asc, desc }
+    public enum inOrderType { ASC, DESC }
     public class SearchParam
     {
         public string SearchText { get; set; } = null;
         public orderByType OrderBy = orderByType.CreatedAt;
-        public inOrderType InOrder = inOrderType.desc;
+        public inOrderType InOrder = inOrderType.DESC;
         public filterRangeType FilterRange = filterRangeType.Pieces;
-        public int RangeMin { get; set; }
+        public int? RangeMin { get; set; } = null;
         public int? RangeMax { get; set; }
     }
 }
